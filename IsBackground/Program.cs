32ef2e745a0c26c3babeaf0613c61b8f28232d14
alexey.tuhtiaev <alexey.tuhtiaev@gmail.com>
2018-02-27 17:23:02 +0300

    IsBackground property
    thread.IsBackground = true

---
+++ IsBackground/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace IsBackground
+{
+    class Program
+    {
+        private static void SomeFunction()
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                Console.Write(".");
+                Thread.Sleep(10);
+            }
+            Console.WriteLine("Second thread finished");
+        }
+
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(SomeFunction);
+            //thread.IsBackground = true;
+
+            thread.Start();
+            Thread.Sleep(500);            
+
+            Console.WriteLine("Main thread finished");
+        }
+    }
+}
